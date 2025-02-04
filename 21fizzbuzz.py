9a495c76e494f976c8d0b10e1992be0df1154f3e
catjobe <catjobe@gmail.com>
2025-02-03 21:48:22 -0800

    update

---
+++ 21fizzbuzz.py
@@ -0,0 +1,7 @@
+# 21fizzbuzz.py by Catrinel Berevoescu
+
+for i in range(1, 101):
+	if i % 3 == 0 and i % 5 == 0: print('FizzBuzz')
+	elif i % 3 == 0: print('Fizz')
+	elif i % 5 == 0: print('Buzz')
+	else: print(i)

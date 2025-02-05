6ac997d90c6045dd18044f635a1fd98623297ced
catjobe <catjobe@gmail.com>
2025-02-05 15:51:17 -0800

    update

---
+++ 23triples.py
@@ -0,0 +1,10 @@
+# 23triples.py by Catrinel Berevoescu
+
+import math
+
+for a in range(1, 100):
+	for b in range(a, 100):
+		c = math.sqrt(a**2 + b**2)
+		if c % 1 == 0:
+			print('a =', a, 'b =', b, 'c =', int(c))
+	
